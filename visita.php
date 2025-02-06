102769d406ce19afc4b50fca226c4b22c074d057
profPane <101067625+profPane@users.noreply.github.com>
2025-02-06 08:46:33 +0000

    qualche aggiustamento qui e li

--- visita.php
+++ visita.php
@@ -13,18 +13,18 @@ if ($conn->connect_error) {
 
 $sql = "SELECT * FROM iscritti;";
 
-$risultati = mysqli_query($conn, $sql);
+$risultati = $conn -> query($sql);
 
 if ($risultati -> num_rows > 0) {
-    echo "La QUERY ha generato: " . $risultati -> num_rows . " righe ";
+  echo "La QUERY ha generato: " . $risultati -> num_rows . " righe ";
   while($row = mysqli_fetch_array($risultati)) { //visita
     $nome = $row['nome'];    
     $cognome = $row['cognome'];
     $email = $row['email'];
-    echo "<br />Nome: ".$cognome." Cognome: ".$nome." email: ".$email;
+    echo "<br />prot: ".$nome." sbang: ".$cognome." banana: ".$email;
   } // visita
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "non ci sono dati con la query data";
 }
 
 $conn->close();
